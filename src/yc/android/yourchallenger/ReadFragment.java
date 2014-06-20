dd886eb5e69f22e80c73be063fe1ffbeb8a32878
Rafa Moreno <rmoreno.cesar@gmail.com>
2014-06-19 21:32:19 -0500

    figunred out mroe or less how to edit fragment ui.

--- src/yc/android/yourchallenger/ReadFragment.java
+++ src/yc/android/yourchallenger/ReadFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ReadFragment extends Fragment {
     public ReadFragment() {
@@ -14,7 +15,8 @@ public class ReadFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
  
         View rootView = inflater.inflate(R.layout.fragment_create, container, false);
- 
+        TextView text = (TextView) rootView.findViewById(R.id.action_settings);
+        text.setText("string");
         return rootView;
     }
  
