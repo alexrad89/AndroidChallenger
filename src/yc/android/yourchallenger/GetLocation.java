7319664477f02a8ef29929953bc1b2073daa290b
Rafa Moreno <rmoreno.cesar@gmail.com>
2014-07-20 14:44:34 -0500

    commit

---
+++ src/yc/android/yourchallenger/GetLocation.java
@@ -0,0 +1,156 @@
+package yc.android.yourchallenger;
+
+
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.util.Log;
+
+public class GetLocation extends Service implements LocationListener{
+	
+	private final Context mContext;
+	boolean isGPSEnabled = false;	
+	boolean isNetworkEnabled = false;	
+	boolean canGetLocation = false;
+	
+	Location location;
+	Double latitude;
+	Double longitude;
+	
+	private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; //10 meters	
+	private static final long  MIN_TIME_BW_UPDATES = 1000 * 60 * 1; //1 minute	
+	protected LocationManager locationManager;
+	
+	public GetLocation (Context context){
+		this.mContext = context;
+		getLocation();
+	}
+	
+	public Location getLocation() {
+		try {
+			locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+			
+			//getting GPS status
+			isGPSEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+			
+			if(!isGPSEnabled && !isNetworkEnabled){
+				//no network provider is enabled
+			} else{
+				this.canGetLocation = true;
+				// First get location from Network provider
+				if (isNetworkEnabled){
+					locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES,
+							MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+					Log.d("Network", "Network");
+					if(locationManager != null){
+						location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+						if(location != null){
+							latitude = location.getLatitude();
+							longitude = location.getLongitude();
+						}
+					}
+				}
+				if (isGPSEnabled){
+					if(location == null){
+						locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+								MIN_TIME_BW_UPDATES,
+								MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+						Log.d("GPS Enabled", "GPS Enabled");
+						if(locationManager != null){
+							location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+							if(location != null){
+								latitude = location.getLatitude();
+								longitude = location.getLongitude();
+							}
+						}
+					}
+				}
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		return location;
+	}
+	/**
+	 * Function to get latitude
+	 * */
+	public double getLatitude(){
+		if(location != null){
+			latitude = location.getLatitude();
+			
+			return 0.0;
+		}
+		
+		return latitude;
+	}
+	public double getLongitude(){
+		if(location != null){
+			longitude = location.getLongitude();
+			return 0.0;
+		}
+		return longitude;
+	}
+	
+	/**
+	 * Function to show setting alert dialog
+	 */
+	public void showSettingsAlert(){
+		AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+		
+		//Setting Dialog Title
+		alertDialog.setTitle("GPS is settings");
+		//Setting Dialog Message
+		alertDialog.setMessage("GPS is not enabled. Do you want to go to settings and enable GPS?"); 
+		//Setting Icon to Dialog
+		//alertDialog.setIcon(R.drawable.delete);
+		//On Pressing Settings button
+		alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+						mContext.startActivity(intent);
+				
+			}
+		});
+		//on pressing cancel button
+		alertDialog.setNegativeButton("Cancel",  new DialogInterface.OnClickListener(){
+			public void onClick(DialogInterface dialog, int which){
+				dialog.cancel();
+			}
+		});
+		//Showing alert message
+		alertDialog.show();
+	}
+	
+	public void stopUsingGPS(){
+		if(locationManager != null){
+			locationManager.removeUpdates(GetLocation.this);
+		}
+	}
+	@Override
+	public void onLocationChanged(Location location){		
+	}
+	@Override
+	public void onProviderDisabled(String provider){		
+	}
+	@Override
+	public void onProviderEnabled(String provider){		
+	}
+	@Override 
+	public void onStatusChanged(String provider, int status, Bundle extras){		
+	}
+	@Override
+	public IBinder onBind(Intent arg0){
+		return null;
+	}	
+}
+
