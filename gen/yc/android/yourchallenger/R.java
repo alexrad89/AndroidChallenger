84fc01367d9985b51956a140fd55958797010065
Rafa Moreno <rmoreno.cesar@gmail.com>
2014-08-12 10:28:30 -0500

    added simple timnder  functionality

--- gen/yc/android/yourchallenger/R.java
+++ gen/yc/android/yourchallenger/R.java
@@ -24,32 +24,61 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int drawer_shadow=0x7f020000;
-        public static final int ic_drawer=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int baby1=0x7f020000;
+        public static final int bg_selector=0x7f020001;
+        public static final int camera=0x7f020002;
+        public static final int cats=0x7f020003;
+        public static final int dislike=0x7f020004;
+        public static final int drawer_shadow=0x7f020005;
+        public static final int face=0x7f020006;
+        public static final int heart=0x7f020007;
+        public static final int ic_drawer=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int ic_like=0x7f02000a;
+        public static final int like=0x7f02000b;
+        public static final int people=0x7f02000c;
+        public static final int puppy=0x7f02000d;
+        public static final int sachin=0x7f02000e;
+        public static final int slideshow=0x7f02000f;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000d;
-        public static final int button=0x7f09000c;
+        public static final int action_settings=0x7f09001b;
+        public static final int button=0x7f090019;
         public static final int content_frame=0x7f090005;
         public static final int create_password=0x7f090008;
         public static final int create_username=0x7f090007;
         public static final int drawer_layout=0x7f090004;
+        public static final int layoutview=0x7f09001a;
         public static final int left_drawer=0x7f090006;
+        public static final int ll_clk_hrts=0x7f090011;
+        public static final int ll_mesg=0x7f09000e;
         public static final int login=0x7f090003;
         public static final int password=0x7f090002;
         public static final int register=0x7f09000a;
         public static final int signup=0x7f090000;
+        public static final int sp_color=0x7f09000b;
+        public static final int sp_hearts=0x7f090013;
+        public static final int sp_image=0x7f09000c;
+        public static final int sp_imageHeart=0x7f090012;
+        public static final int sp_imageLike=0x7f090016;
+        public static final int sp_likes=0x7f090017;
+        public static final int sp_linh=0x7f09000d;
+        public static final int sp_ll_like_layout=0x7f090015;
+        public static final int sp_msg=0x7f09000f;
         public static final int textView1=0x7f090009;
-        public static final int txtLabel2=0x7f09000b;
+        public static final int txtLabel2=0x7f090018;
         public static final int user_name=0x7f090001;
+        public static final int view1=0x7f090014;
+        public static final int view2=0x7f090010;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_sign_up=0x7f030002;
-        public static final int fragment_create=0x7f030003;
-        public static final int nav_drawer=0x7f030004;
+        public static final int custom_layout=0x7f030003;
+        public static final int fragment_create=0x7f030004;
+        public static final int mainlayout=0x7f030005;
+        public static final int nav_drawer=0x7f030006;
     }
     public static final class menu {
         public static final int data_base=0x7f080000;
--- src/yc/android/yourchallenger/MainActivity.java
+++ src/yc/android/yourchallenger/MainActivity.java
@@ -1,18 +1,32 @@
 package yc.android.yourchallenger;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
  
@@ -23,10 +37,22 @@ public class MainActivity extends Activity {
 	private CharSequence mDrawerTitle;
 	private CharSequence mTitle;
 	
+	//TINDER VARIABLES
+	int windowwidth;
+	int screenCenter;
+	int x_cord, y_cord;
+	int Likes = 0;
+	RelativeLayout parentView;
+	float alphaValue = 0;
+	private Context m_context;
+
+	@SuppressWarnings("deprecation")
+	@SuppressLint("NewApi")
 	 public void onCreate(Bundle savedInstanceState){
 		 super.onCreate(savedInstanceState);
 		 setContentView(R.layout.activity_main);
 		 
+		 //NAV DRAWER BEGIN
 		 mNavigationDrawerItemTitles = getResources().getStringArray(R.array.navigation_drawer_items_array);
 	        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 	        mDrawerList = (ListView) findViewById(R.id.left_drawer);
@@ -63,8 +89,171 @@ public class MainActivity extends Activity {
 	        // Set the drawer toggle as the DrawerListener
 	        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
-	        getActionBar().setDisplayHomeAsUpEnabled(true);
-	        getActionBar().setHomeButtonEnabled(true);	       
+	       getActionBar().setDisplayHomeAsUpEnabled(true);
+	       getActionBar().setHomeButtonEnabled(true);	
+	       
+	     //TINDER-LIKE BEGIN
+			 //
+				setContentView(R.layout.mainlayout);
+				m_context = MainActivity.this;
+
+				parentView = (RelativeLayout) findViewById(R.id.layoutview);
+				windowwidth = getWindowManager().getDefaultDisplay().getWidth();
+				screenCenter = windowwidth / 2;
+				int[] myImageList = new int[] { R.drawable.cats, R.drawable.baby1,
+						R.drawable.sachin, R.drawable.cats, R.drawable.puppy };
+
+				for (int i = 0; i < 5; i++) {
+					LayoutInflater inflate = (LayoutInflater) m_context
+							.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+					final View m_view = inflate.inflate(R.layout.custom_layout, null);
+					ImageView m_image = (ImageView) m_view.findViewById(R.id.sp_image);
+					LinearLayout m_topLayout = (LinearLayout) m_view
+							.findViewById(R.id.sp_color);
+					LinearLayout m_bottomLayout = (LinearLayout) m_view
+							.findViewById(R.id.sp_linh);
+					// final RelativeLayout myRelView = new RelativeLayout(this);
+					m_view.setLayoutParams(new LayoutParams((windowwidth - 80), 450));
+					m_view.setX(40);
+					m_view.setY(40);
+					m_view.setTag(i);
+					m_image.setBackgroundResource(myImageList[i]);
+
+					if (i == 0) {
+						m_view.setRotation(-1);
+					} else if (i == 1) {
+						m_view.setRotation(-5);
+
+					} else if (i == 2) {
+						m_view.setRotation(3);
+
+					} else if (i == 3) {
+						m_view.setRotation(7);
+
+					} else if (i == 4) {
+						m_view.setRotation(-2);
+
+					} else if (i == 5) {
+						m_view.setRotation(5);
+
+					}
+
+					// ADD dynamically like button on image.
+					final Button imageLike = new Button(m_context);
+					imageLike.setLayoutParams(new LayoutParams(100, 50));
+					imageLike.setBackgroundDrawable(getResources().getDrawable(
+							R.drawable.like));
+					imageLike.setX(20);
+					imageLike.setY(-250);
+					imageLike.setAlpha(alphaValue);
+					m_topLayout.addView(imageLike);
+
+					// ADD dynamically dislike button on image.
+					final Button imagePass = new Button(m_context);
+					imagePass.setLayoutParams(new LayoutParams(100, 50));
+					imagePass.setBackgroundDrawable(getResources().getDrawable(
+							R.drawable.dislike));
+
+					imagePass.setX(260);
+					imagePass.setY(-300);
+					imagePass.setAlpha(alphaValue);
+					m_topLayout.addView(imagePass);
+
+					// Click listener on the bottom layout to open the details of the
+					// image.
+					//m_bottomLayout.setOnClickListener(new OnClickListener() {
+
+						//@Override
+						//public void onClick(View v) {
+							//startActivity(new Intent(m_context, DetailsActivity.class));
+
+						//}
+					//});
+
+					// Touch listener on the image layout to swipe image right or left.
+					m_topLayout.setOnTouchListener(new OnTouchListener() {
+
+						@Override
+						public boolean onTouch(View v, MotionEvent event) {
+							x_cord = (int) event.getRawX();
+							y_cord = (int) event.getRawY();
+
+							m_view.setX(x_cord - screenCenter + 40);
+							m_view.setY(y_cord - 150);
+							switch (event.getAction()) {
+							case MotionEvent.ACTION_DOWN:
+								break;
+							case MotionEvent.ACTION_MOVE:
+								x_cord = (int) event.getRawX();
+								y_cord = (int) event.getRawY();
+								m_view.setX(x_cord - screenCenter + 40);
+								m_view.setY(y_cord - 150);
+								if (x_cord >= screenCenter) {
+									m_view.setRotation((float) ((x_cord - screenCenter) * (Math.PI / 32)));
+									if (x_cord > (screenCenter + (screenCenter / 2))) {
+										imageLike.setAlpha(1);
+										if (x_cord > (windowwidth - (screenCenter / 4))) {
+											Likes = 2;
+										} else {
+											Likes = 0;
+										}
+									} else {
+										Likes = 0;
+										imageLike.setAlpha(0);
+									}
+									imagePass.setAlpha(0);
+								} else {
+									// rotate
+									m_view.setRotation((float) ((x_cord - screenCenter) * (Math.PI / 32)));
+									if (x_cord < (screenCenter / 2)) {
+										imagePass.setAlpha(1);
+										if (x_cord < screenCenter / 4) {
+											Likes = 1;
+										} else {
+											Likes = 0;
+										}
+									} else {
+										Likes = 0;
+										imagePass.setAlpha(0);
+									}
+									imageLike.setAlpha(0);
+								}
+
+								break;
+							case MotionEvent.ACTION_UP:
+								x_cord = (int) event.getRawX();
+								y_cord = (int) event.getRawY();
+
+								Log.e("X Point", "" + x_cord + " , Y " + y_cord);
+								imagePass.setAlpha(0);
+								imageLike.setAlpha(0);
+
+								if (Likes == 0) {
+									// Log.e("Event Status", "Nothing");
+									m_view.setX(40);
+									m_view.setY(40);
+									m_view.setRotation(0);
+								} else if (Likes == 1) {
+									// Log.e("Event Status", "Passed");
+									parentView.removeView(m_view);
+								} else if (Likes == 2) {
+
+									// Log.e("Event Status", "Liked");
+									parentView.removeView(m_view);
+								}
+								break;
+							default:
+								break;
+							}
+							return true;
+						}
+					});
+
+					parentView.addView(m_view);
+
+				}
+			 //TINDER-LIKE END
 	}
 	 
 	 @Override
@@ -116,9 +305,9 @@ public class MainActivity extends Activity {
 		 default:
 			 break;
 		 }
-		/* Bundle args = new Bundle();
+		 Bundle args = new Bundle();
 		 args.putInt("key", position);
-		 fragment.setArguments(args);*/
+		 fragment.setArguments(args);
 		if(fragment != null){ 
 			 FragmentManager fragmentManager = getFragmentManager();
 			    fragmentManager.beginTransaction()
@@ -131,12 +320,12 @@ public class MainActivity extends Activity {
 		}
 	 }	 
 	 
+	 
+	
 	 @Override
 	 public void setTitle(CharSequence title){
 		 mTitle = title;
 		 getActionBar().setTitle(mTitle);
 	 }
-	
-	
 }
 
